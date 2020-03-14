ec09f039e0610fbb65a37cd9583eb1314521fddc
Iris Martinez <irismartinez114@gmail.com>
2020-03-14 11:50:01 -0700

    Adding eslint

--- server/index.js
+++ server/index.js
@@ -7,10 +7,10 @@ const PORT = 1234;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(express.static(__dirname + '/../client/dist'));
+app.use(express.static(__dirname + '/../client/dist/'));
 
 app.get('/', (req, res) => {
-	res.render('/index');
+	res.render('index');
 });
 
 
