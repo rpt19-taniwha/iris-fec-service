69387a07dc94852972db9ab831b7f8346fa17d05
Iris Martinez <irismartinez114@gmail.com>
2020-03-17 20:31:18 -0700

    API endpoints

---
+++ db/index.js
@@ -0,0 +1,30 @@
+const mysql = require('mysql');
+const mysqlConfig = require('./config.js');
+
+const connection = mysql.createConnection(mysqlConfig);
+connection.connect();
+
+
+const getProductReviews = function(productId, callback) {
+	connection.query(`SELECT * FROM product_reviews WHERE product_id=${productId}`, (error, results, fields) => {
+    if (error){
+      callback(null, error)
+    } else {
+      callback(results, null)
+    }
+  });
+}
+
+const getStoreReviews = function(productId, callback) {
+	connection.query(`SELECT * FROM store_reviews WHERE product_id=${productId}`, (error, results, fields) => {
+    if (error){
+      callback(null, error)
+    } else {
+      callback(results, null)
+    }
+  });
+}
+
+module.exports = {
+  getProductReviews
+};
--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path')
+const db = require('../db');
 
 const app = express();
 const PORT = 1234;
@@ -10,7 +12,30 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/../client/dist/'));
 
 app.get('/', (req, res) => {
-	res.render('index');
+	res.render('index')
+});
+
+app.get('/:id/product/reviews', (req, res) => {
+	let productId = req.params.id;
+	db.getProductReviews(productId, (result, error) => {
+		if (error) {
+			console.log(error)
+		} else {
+			res.send(result)
+		}
+	})
+});
+
+app.get('/:id/store/reviews', (req, res) => {
+	let storeId = req.params.id;
+	db.getStoreReviews(storeId, (result, error) => {
+		if (error) {
+			console.log(error)
+		} else {
+			res.send(result)
+		}
+	})
+
 });
 
 
